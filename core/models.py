3ef576c6ccbac9464d14c01885e927c2cda7d281
keclements <kristin.clements@gmail.com>
2019-03-20 17:42:32 -0400

    added product model

--- core/models.py
+++ core/models.py
@@ -1,4 +1,21 @@
 from django.db import models
 
 # Create your models here.
-# product model
+class Product(models.Model):
+
+	# Fields
+	product_name = models.CharField(max_length=60, help_text='Enter product field name')
+	# product_price = models.IntegerField()
+	# condition = 
+	
+	# Metadata
+	class Meta:
+		ordering =['-my_field_name'] 
+		# will need to fix the ordering it may be diff for project
+
+
+	__str__(self)
+        return self.product_name
+
+
+
