3426d03013ed115506cb85b7b305e7784a0632c9
keclements <kristin.clements@gmail.com>
2019-03-21 00:15:24 -0400

    models added

--- core/models.py
+++ core/models.py
@@ -1,21 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+User = get_user_model()
+
 class Product(models.Model):
+	EXCELLENT = 'ED'
 
-	# Fields
-	product_name = models.CharField(max_length=60, help_text='Enter product field name')
-	# product_price = models.IntegerField()
-	# condition = 
-	
-	# Metadata
-	class Meta:
-		ordering =['-my_field_name'] 
-		# will need to fix the ordering it may be diff for project
+	PRODCUT_CONDITIONS = (
+		(EXCELLENT, 'Excelent'),
+		(VERY)
+	)
+
+	"""Contains information related to the Product for sale"""
+	product_name = models.CharField(max_length=100, unique=True, help_text="Enter product name")
+	product_description = models.CharField(max_length=250, blank=True, null=True)
+	price = models.IntegerField(max_length=10, blank=True, null=True, help_text="Enter integer price")
+	category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+	condition = models
+	# class Meta:
+	# 	ordering =['-my_field_name'] 
+
+	def __str__(self):
+		return self.name
 
 
-	__str__(self)
-        return self.product_name
-
-
-
+class ProductCategory(models.Model):
+	""
