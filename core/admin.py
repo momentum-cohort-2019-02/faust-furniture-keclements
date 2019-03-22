cef216924ba69a1cf24f05a63cb88ce98d77ca45
keclements <kristin.clements@gmail.com>
2019-03-21 23:17:39 -0400

    drafted views.py

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from core.models import Product
+from core.models import Category, Product
 
-#@admin.register(Product)
+admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ['name', 'slug']
+	prepopulated_fields = {'slug': ('name',)}
+
+admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available',]
+    list_filter = ['available', ]
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name', )}
+
+	# use prepopulated_fields attribute to specify fields were the value is automatically set using value of other fields
--- core/models.py
+++ core/models.py
@@ -1,49 +1,46 @@
 from django.db import models
+from slug import AutoSlugField
 
 # Create your models here.
 
 #User = get_user_model()
 
-class Product(models.Model):
-	# EXCELLENT = 'EX'
-	# VERY_GOOD = 'VG'
-	# GOOD = 'GD'
-	# AVERAGE = 'AV'
-
-	# PRODUCT_CONDITIONS = (
-	# 	(EXCELLENT, 'Excelent'),
-	# 	(VERY_GOOD, 'Very Good'),
-	# 	(GOOD, 'Good'),
-	# 	(AVERAGE, 'Average')
-	# )
-
-	# """Contains information related to the Product for sale"""
-	# product_name = models.CharField(max_length=10, unique=True, help_text="Enter product name")
-	# product_description = models.CharField(max_length=250, blank=True, null=True)
-	# price = models.IntegerField(blank=True, null=True, help_text="Enter integer price")
-	# category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
-	# condition = models.CharField(max_length=2, choices=PRODUCT_CONDITIONS)
-	# # product_dimention = models.
-
-	# #def product_conditions(self)
-	# 	#conditions = [] 
-
-	# def __str__(self):
-	# 	return self.product_name
-
 class Category(models.Model):
-	name = models.CharField(max_length=200, db_index=True)
-	slug = models.SlugField(max_length=200, unique=True)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = AutoSlugField(
+        unique=True,
+        populate_from="name",
+        blank=True,
+        null=True,
+    )
 
-	class Meta:
-		ordering = ('name', )
-		verbose_name = 'category'
-		verbose_name_plural = 'categories'
+    class Meta:
+        ordering = ('name', )
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
 
-		def __str__(self):
-			return.self.name
+    def __str__(self):
+        return f'{self.name}
 
+class Product(models.Model):
+    """Contains information related to the Product for sale"""
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, unique=True, help_text="Enter product name")
+    slug = AutoSlugField(unique=True, populate_from="name", blank=True, null=True,)
+    description = models.TextField(max_length=300, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, help_text="Enter integer price")
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    #condition = models.CharField(max_length=2, choices=PRODUCT_CONDITIONS)
+    available = models.BooleanField(default=True)
+    # product_dimention = models.
 
+    class Meta:
+        ordering = ('name', )
+        index_together = (('id', 'slug'),
+        )
 
+    def __str__(self):
+        return self.name
 		
+#     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
 
